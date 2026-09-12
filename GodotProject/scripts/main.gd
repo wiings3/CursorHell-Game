3429291064eab8012f52b59a24a3ff0f65996569
wiings3 <115431054+wiings3@users.noreply.github.com>
2026-09-12 17:18:57 -0400

    Register Level 8 Dead Zones

--- GodotProject/scripts/main.gd
+++ GodotProject/scripts/main.gd
@@ -8,7 +8,8 @@ const LEVEL_SCENES: Array[PackedScene] = [
 	preload("res://Scenes/Levels/Level4.tscn"),
 	preload("res://Scenes/Levels/Level5.tscn"),
 	preload("res://Scenes/Levels/Level6.tscn"),
-	preload("res://Scenes/Levels/Level7.tscn")
+	preload("res://Scenes/Levels/Level7.tscn"),
+	preload("res://Scenes/Levels/Level8.tscn")
 ]
 
 @export var starting_level_index: int = 0
